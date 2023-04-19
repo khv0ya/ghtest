11e65fa64bcb731ba8fd116dd99ed35db75b544b
Yuliya <y.novikova.653778302@gmail.com>
2023-04-19 14:48:38 +0200

    added a sample test

---
+++ src/test/java/org/example/unit/SampleTest.java
@@ -0,0 +1,14 @@
+package org.example.unit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        //  This test needs to be fixed!!!
+        assertTrue(false);
+    }
+}
